02f902285c3d3eb03afbb868ba7161524b31bf52
danbogatov <123800398+danbogatov@users.noreply.github.com>
2023-02-01 13:11:00 +0300

    Add files via upload

---
+++ autotest/src/test/java/org/example/LoginPage.java
@@ -0,0 +1,44 @@
+package org.example;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+public class LoginPage {
+    /**
+     * конструктор класса, занимающийся инициализацией полей класса
+     */
+    public WebDriver driver;
+    public LoginPage(WebDriver driver) {
+        PageFactory.initElements(driver, this);
+        this.driver = driver; }
+    /**
+     * определение локатора поля ввода логина
+     */
+    @FindBy(xpath = "/html/body/div/div/form/input[1]")
+    private WebElement loginField;
+    /**
+     * определение локатора кнопки входа в аккаунт
+     */
+    @FindBy(xpath = "/html/body/div/div/form/button")
+    private WebElement loginBtn;
+    /**
+     * определение локатора поля ввода пароля
+     */
+    @FindBy(xpath = "/html/body/div/div/form/input[2]")
+    private WebElement passwdField;
+    /**
+     * метод для ввода логина
+     */
+    public void inputLogin(String login) {
+        loginField.sendKeys(login); }
+    /**
+     * метод для ввода пароля
+     */
+    public void inputPasswd(String passwd) {
+        passwdField.sendKeys(passwd); }
+    /**
+     * метод для осуществления нажатия кнопки входа в аккаунт
+     */
+    public void clickLoginBtn() {
+        loginBtn.click(); }
+}
